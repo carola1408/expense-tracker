d985162ffd2452eb0c2e8ee80860169ff352a68b
user <carola1408@yahoo.com.tw>
2023-04-01 17:02:59 +0800

    feat: modify create function

--- app.js
+++ app.js
@@ -40,14 +40,14 @@ app.get('/', (req, res) => {
 })
 
 // new record page
-app.get('/new', (req, res) => {
+app.get('/records/new', (req, res) => {
   return res.render('new')
 })
 
 // create new record
-app.post('/', (req, res) => {
+app.post('/records', (req, res) => {
   const { name, date, amount, categoryId } = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-  return Record.create({ name, date, amount, categoryId })     // 存入資料庫
+  return Record.create({ ...req.body })     // 存入資料庫
     .then(() => res.redirect('/')) // 新增完成後導回首頁
     .catch(error => console.log(error))
 })
