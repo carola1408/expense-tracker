dab013160bf8449085fc45d27790a417c9b47a1f
user <carola1408@yahoo.com.tw>
2023-04-09 11:04:56 +0800

    refactor: modify categorySeeder and recordSeeder

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose') // 載入 mongoose
 
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+
 // 設定連線到 mongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -15,3 +21,5 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 })
+
+module.exports = db
--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,12 +1,7 @@
-const mongoose = require('mongoose') // 載入 mongoose
 const Category = require('../category') // 載入 category model
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-// 取得資料庫連線狀態
-const db = mongoose.connection
+const db = require('../../config/mongoose') // 載入 mongoose
+
+
 const CATEGORY = {
   家居物業: 'fa-solid fa-house',
   交通出行: 'fa-solid fa-van-shuttle',
@@ -15,14 +10,7 @@ const CATEGORY = {
   其他: 'fa-solid fa-pen',
 }
 
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+
 //新增資料
 db.once('open', () => {
   console.log('mongodb connected!')
