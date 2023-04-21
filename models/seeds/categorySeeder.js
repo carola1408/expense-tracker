c1eebad1d735d4230e5ed25e507023bcb24615bf
user <carola1408@yahoo.com.tw>
2023-04-21 11:12:08 +0800

    modify record and category seeder

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,24 +1,30 @@
 const Category = require('../category') // 載入 category model
 const db = require('../../config/mongoose') // 載入 mongoose
-
-
-const CATEGORY = {
-  家居物業: 'fa-solid fa-house',
-  交通出行: 'fa-solid fa-van-shuttle',
-  休閒娛樂: 'fa-solid fa-face-grin-beam',
-  餐飲食品: 'fa-solid fa-utensils',
-  其他: 'fa-solid fa-pen',
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
+
+const categoryList = [
+  { id: 1, name: "家居物業" },
+  { id: 2, name: "交通出行" },
+  { id: 3, name: "休閒娛樂" },
+  { id: 4, name: "餐飲食品" },
+  { id: 5, name: "其他" }
+
+]
+
 
 
 //新增資料
-db.once('open', () => {
-  console.log('mongodb connected!')
-  for (const [name, icon] of Object.entries(CATEGORY)) {
-    Category.create({
-      name,
-      icon,
+db.once("open", () => {
+  return Promise.all(
+    categoryList.map((category) => {
+      return Category.create(category);
     })
-  }
-  console.log('done')
+  )
+    .then(() => {
+      console.log("Category seeder is done!");
+      process.exit();
+    })
+    .catch((error) => console.log(error));
 })
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,18 +1,84 @@
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const bcrypt = require("bcryptjs") // 載入 bcryptjs model
 const Record = require('../record') // 載入 record model
 const db = require('../../config/mongoose') // 載入 mongoose
+const User = require("../user") // 載入 user
+
 
 
 // 載入 JSON
 const recordList = require('../../records.json').results
 
+const SEED_USER = [{
+  name: '廣志',
+  email: 'user1@test.com',
+  password: '12345678',
+  recordIndex: [0, 2, 4]
+},
+{
+  name: '小新',
+  email: 'user2@test.com',
+  password: '12345678',
+  recordIndex: [2, 3]
+}
+]
+const SEED_RECORD = [
+  {
+    name: "午餐",
+    date: "2019-04-23",
+    amount: 60,
+    categoryId: 4
+  },
+  {
+    name: "晚餐",
+    date: "2019-04-23",
+    amount: 60,
+    categoryId: 4
+  },
+  {
+    name: "捷運",
+    date: "2019-04-23",
+    amount: 120,
+    categoryId: "2"
+  },
+  {
+    name: "電影：驚奇隊長",
+    date: "2019-04-23",
+    amount: 220,
+    categoryId: "3"
+  },
+  {
+    name: "租金",
+    date: "2015-04-01",
+    amount: 25000,
+    categoryId: "1"
+  },
+]
+
+
 //新增資料
-db.once('open', () => {
-  console.log('mongodb connected!')
-  Record.create(recordList)
-    .catch(err => {
-      console.trace(err)
+db.once("open", () => {
+  return Promise.all(
+    SEED_USER.map((user) => {
+      const { name, email, password, recordIndex } = user
+      return User.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcrypt.genSaltSync(10)),
+      }).then((user) => {
+        const userId = user._id;
+        const records = recordIndex.map((index) => {
+          return { ...SEED_RECORD[index], userId };
+        });
+        return Record.create(records)
+      })
     })
-    .finally(() => {
-      db.close()
+  )
+    .then(() => {
+      console.log("Record seeder is done!")
+      process.exit()
     })
 })
