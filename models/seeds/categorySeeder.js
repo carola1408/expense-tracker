ef456b646295a2acc34938d385fe4b2123c9a3f4
user <carola1408@yahoo.com.tw>
2023-05-03 11:10:44 +0800

    modify categorySeed imageUrl

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -5,11 +5,11 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const categoryList = [
-  { id: 1, name: "家居物業:fa-house" },
-  { id: 2, name: "交通出行:fa-van-shuttle" },
-  { id: 3, name: "休閒娛樂:fa-face-grin-beam" },
-  { id: 4, name: "餐飲食品:fa-utensils" },
-  { id: 5, name: "其他:fa-pen" }
+  { id: 1, name: "家居物業", imageUrl: 'fa-solid fa-house' },
+  { id: 2, name: "交通出行", imageUrl: 'fa-solid fa-van-shuttle' },
+  { id: 3, name: "休閒娛樂", imageUrl: 'fa-solid fa-face-grin-beam' },
+  { id: 4, name: "餐飲食品", imageUrl: 'fa-solid fa-utensils' },
+  { id: 5, name: "其他", imageUrl: 'fa-solid fa-pen' }
 
 ]
 
