852d139d3ed9f8e00ed4e6fe5f6bde5d930f0967
user <carola1408@yahoo.com.tw>
2023-05-12 10:05:02 +0800

    delete helper

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,29 +1,32 @@
 const Category = require('../category') // 載入 category model
 const db = require('../../config/mongoose') // 載入 mongoose
+const seedCategory = require('./category.json')
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-const SEED_CATEGORY = [
-  { id: 1, name: "家居物業" },
-  { id: 2, name: "交通出行" },
-  { id: 3, name: "休閒娛樂" },
-  { id: 4, name: "餐飲食品" },
-  { id: 5, name: "其他" },
-]
 
 
 //新增資料
-db.once("open", () => {
-  return Promise.all(
-    SEED_CATEGORY.map((category) => {
-      return Category.create(category);
-    })
-  )
-    .then(() => {
-      console.log("Category seeder is done!");
-      process.exit();
-    })
-    .catch((error) => console.log(error));
-})
+// db.once("open", () => {
+//   return Promise.all(
+//     seedCategory.map((category) => {
+//       return Category.create(category);
+//     })
+//   )
+//     .then(() => {
+//       console.log("Category seeder is done!");
+//       process.exit();
+//     })
+//     .catch((error) => console.log(error));
+// })
 
+db.once('open', () => {
+  Promise.all(seedCategory.map(item => {
+    return Category.create({ ...item })
+  })
+  )
+    .then(data => console.log('Done = ', data))
+    .catch(console.error)
+    .finally(() => process.exit())
+})
