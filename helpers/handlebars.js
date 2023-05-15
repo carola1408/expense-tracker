2d1ce05c9779d381067fcd4d202f0122b01d9ab1
user <carola1408@yahoo.com.tw>
2023-05-15 11:29:42 +0800

    modify again app.js and hom route and add helpers

---
+++ helpers/handlebars.js
@@ -0,0 +1,18 @@
+const Handlebars = require("handlebars");
+const imageUrls = {
+  1: ["家居物業", "fa-solid fa-house"],
+  2: ["交通出行", "fa-solid fa-van-shuttle"],
+  3: ["休閒娛樂", "fa-solid fa-face-grin-beam"],
+  4: ["餐飲食品", "fa-solid fa-utensils"],
+  5: ["其他", "fa-solid fa-pen"],
+};
+
+Handlebars.registerHelper("imageUrl", function (num) {
+  return imageUrls[num][1]
+})
+
+module.exports = {
+  ifCond: function (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this);
+  }
+}
