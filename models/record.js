f947a2661808cd392ea269ce4ba22ac2b7507427
user <carola1408@yahoo.com.tw>
2023-05-19 23:24:30 +0800

    feat: modify recordSeeder.js

--- models/record.js
+++ models/record.js
@@ -1,33 +1,4 @@
-// const mongoose = require('mongoose') // 載入 mongoose
-// const Schema = mongoose.Schema
-// const recordSchema = new Schema({
-//   name: {
-//     type: String,
-//     required: true
-//   },
-//   date: {
-//     type: Date,
-//     required: true
-//   },
-//   amount: {
-//     type: Number,
-//     required: true
-//   },
-//   category: {
-//     type: String,
-//     required: true
-//   },
-//   userId: {  // 加入關聯設定
-//     type: Schema.Types.ObjectId,
-//     ref: 'User',
-//     index: true,
-//     required: true
-//   }
-// })
-
-//透過 module.exports 輸出
-// module.exports = mongoose.model('Record', recordSchema)
-const mongoose = require('mongoose')
+const mongoose = require('mongoose') // 載入 mongoose
 const Schema = mongoose.Schema
 const recordSchema = new Schema({
   name: {
